bec3f0d727b022c55f78a5e168e773aeee6d724e
Amy-Xu <amy.xu@aei.org>
2015-08-21 15:09:30 -0400

    revert it back

--- Stage II/Single_Year_Stage_II.py
+++ Stage II/Single_Year_Stage_II.py
@@ -1,7 +1,7 @@
 
 import numpy as np
 import pandas as pd
-from numba import vectorize, float64
+from pandas import DataFrame
 from cylp.cy import CyClpSimplex
 from cylp.py.modeling.CyLPModel import CyLPArray, CyLPModel
 
@@ -12,13 +12,54 @@ def Single_Year_Stage_II(puf, Stage_I_factors, Stage_II_targets, year, tol):
 
 
     print("Preparing coefficient matrix...")
-    apopsnr = pd.Series([Stage_I_factors[year]["APOPSNR"]] * length)
-    arets = pd.Series([Stage_I_factors[year]["ARETS"]] * length)
+
+    s006 = np.where(puf.e02400>0,
+                    puf.s006*Stage_I_factors[year]["APOPSNR"]/100,
+                    puf.s006*Stage_I_factors[year]["ARETS"]/100)
+
+
+
+    single_return = np.where(puf.mars==1, s006, 0)
+    joint_return = np.where((puf.mars==2)|(puf.mars==3), s006, 0)
+    hh_return = np.where(puf.mars==4,s006,0)
+    return_w_SS = np.where(puf.e02400>0,s006,0)
+
+    dependent_exempt_num = (puf.xocah+puf.xocawh+puf.xoodep+puf.xopar)*s006
+    interest = puf.e00300*s006
+    dividend = puf.e00600*s006
+    biz_income = np.where(puf.e00900>0, puf.e00900, 0)*s006
+    biz_loss = np.where(puf.e00900<0, -puf.e00900, 0)*s006
+    cap_gain = np.where(puf.e01000>0, puf.e01000, 0)*s006
+    annuity_pension = puf.e01700*s006
+    sch_e_income = np.where(puf.e02000>0, puf.e02000, 0)*s006
+    sch_e_loss = np.where(puf.e02000<0, -puf.e02000, 0)*s006
+    ss_income = puf.e02400*s006
+    unemployment_comp = puf.e02300*s006
+
+
+    # Wage distribution
+
+    wage_1 = np.where(puf.e00100<=0, puf.e00200,0)*s006
+    wage_2 = np.where((puf.e00100>0)&(puf.e00100<=10000), puf.e00200,0)*s006
+    wage_3 = np.where((puf.e00100>10000)&(puf.e00100<=20000), puf.e00200,0)*s006
+    wage_4 = np.where((puf.e00100>20000)&(puf.e00100<=30000), puf.e00200,0)*s006
+    wage_5 = np.where((puf.e00100>30000)&(puf.e00100<=40000), puf.e00200,0)*s006
+    wage_6 = np.where((puf.e00100>40000)&(puf.e00100<=50000), puf.e00200,0)*s006
+    wage_7 = np.where((puf.e00100>50000)&(puf.e00100<=75000), puf.e00200,0)*s006
+    wage_8 = np.where((puf.e00100>75000)&(puf.e00100<=100000), puf.e00200,0)*s006
+    wage_9 = np.where((puf.e00100>100000)&(puf.e00100<=200000), puf.e00200,0)*s006
+    wage_10 = np.where((puf.e00100>200000)&(puf.e00100<=500000), puf.e00200,0)*s006
+    wage_11 = np.where((puf.e00100>500000)&(puf.e00100<=1000000), puf.e00200,0)*s006
+    wage_12 = np.where((puf.e00100>1000000), puf.e00200,0)*s006
+
 
     # Set up the matrix
-    One_half_LHS = np.vstack(blow_up(puf.e00100, puf.e00200, puf.e00300, puf.e00600, puf.e00900, puf.e01000,
-                                     puf.e01700, puf.e02000, puf.e02300, puf.e02400, puf.s006, puf.mars,
-                                     puf.xocah, puf.xocawh, puf.xoodep, puf.xopar, apopsnr, arets))
+    One_half_LHS = np.vstack((single_return, joint_return, hh_return, return_w_SS,
+                              dependent_exempt_num, interest, dividend,
+                              biz_income,biz_loss, cap_gain, annuity_pension,
+                              sch_e_income, sch_e_loss, ss_income, unemployment_comp,
+                              wage_1, wage_2, wage_3, wage_4, wage_5, wage_6,
+                              wage_7, wage_8, wage_9, wage_10, wage_11, wage_12))
 
 
     # Coefficients for r and s
@@ -30,11 +71,14 @@ def Single_Year_Stage_II(puf, Stage_I_factors, Stage_II_targets, year, tol):
 
     APOPN = Stage_I_factors[year]["APOPN"]
 
-    Single = Stage_II_targets[year]['Single']-single_return.sum()
-    Joint = Stage_II_targets[year]['Joint']-joint_return.sum()
-    HH = Stage_II_targets[year]['HH']-hh_return.sum()
-    SS_return = Stage_II_targets[year]['SS_return']-return_w_SS.sum()
-    Dep_return = Stage_II_targets[year]['Dep_return'] -  dependent_exempt_num.sum()
+    b = []
+
+    b.append(Stage_II_targets[year]['Single']-single_return.sum())
+    b.append(Stage_II_targets[year]['Joint']-joint_return.sum())
+    b.append(Stage_II_targets[year]['HH']-hh_return.sum())
+    b.append(Stage_II_targets[year]['SS_return']-return_w_SS.sum())
+
+    b.append(Stage_II_targets[year]['Dep_return'] -  dependent_exempt_num.sum())
 
     AINTS = Stage_I_factors[year]["AINTS"]
     INTEREST = Stage_II_targets[year]['INTS']*APOPN/AINTS*1000-interest.sum()
@@ -84,13 +128,10 @@ def Single_Year_Stage_II(puf, Stage_I_factors, Stage_II_targets, year, tol):
 
 
 
-    temp = [Single, Joint, HH, SS_return, Dep_return,
-            INTEREST,DIVIDEND, BIZ_INCOME, BIZ_LOSS, CAP_GAIN, ANNUITY_PENSION,
-            SCH_E_INCOME, SCH_E_LOSS, SS_INCOME, UNEMPLOYMENT_COMP,
-            WAGE_1,WAGE_2, WAGE_3,WAGE_4, WAGE_5, WAGE_6,
-            WAGE_7,WAGE_8,WAGE_9, WAGE_10, WAGE_11, WAGE_12]
-    
-    b = list(temp)
+    temp = [INTEREST,DIVIDEND, BIZ_INCOME, BIZ_LOSS, CAP_GAIN, ANNUITY_PENSION, SCH_E_INCOME, SCH_E_LOSS, SS_INCOME, UNEMPLOYMENT_COMP,
+            WAGE_1,WAGE_2, WAGE_3,WAGE_4, WAGE_5, WAGE_6, WAGE_7,WAGE_8,WAGE_9, WAGE_10, WAGE_11, WAGE_12]
+    for m in temp:
+        b.append(m)
 
     targets = CyLPArray(b)
     print("Targets for year ", year, " is ", targets)
@@ -124,119 +165,6 @@ def Single_Year_Stage_II(puf, Stage_I_factors, Stage_II_targets, year, tol):
     z = np.empty([length])
     z = (1+model.primalVariableSolution['r'] - model.primalVariableSolution['s'])*s006
     return z
-
-@vectorize([float64(float64, float64, float64, float64, float64, float64,
-                    float64, float64, float64, float64, float64, float64,
-                    float64, float64, float64, float64, float64, float64)])
-def blow_up(e00100, e00200, e00300, e00600, e00900, e01000,
-            e01700, e02000, e02300, e02400, s006, mars, xocah,
-            xocawh, xoodep, xopar, apopsnr, arets):
-
-    # blow up the weights
-    if e02400 > 0:
-        s006 = s006 * apopsnr / 100
-    else:
-        s006 = s006 * arets / 100
-    
-    single_return = 0
-    joint_return = 0
-    hh_return = 0
-    if mars == 1:
-        single_return = s006
-    elif mars == 2 or mars == 3:
-        joint_return = s006
-    else:
-        hh_return = s006
-        
-    if e02400 > 0:
-        ss_return = s006
-    else:
-        ss_return = 0
-        
-    dependent_exempt_num = (xocah + xocawh + xoodep + xopar) * s006
-    interest = e00300 * s006
-    dividend = e00600 * s006
-    
-    biz_income = 0
-    biz_loss = 0
-    if e00900 > 0:
-        biz_income = e00900 * s006
-    else:
-        biz_loss = -e00900 * s006
-    
-    
-    if e01000 > 0:
-        cap_gain = e01000 * s006
-    else:
-        cap_gain = 0
-        
-    
-    annuity_pension = e01700 * s006
-    
-    sch_e_income = 0
-    sch_e_loss = 0
-    if e02000:
-        sch_e_income = e02000 * s006
-    else:
-        sch_e_loss = -e02000 * s006
-
-    ss_income = e02400 * s006
-    
-    unemployment_comp = e02300 * s006
-
-
-    # Wage distribution
-    wage_1 = 0
-    wage_2 = 0
-    wage_3 = 0
-    wage_4 = 0
-    wage_5 = 0
-    wage_6 = 0
-    wage_7 = 0
-    wage_8 = 0
-    wage_9 = 0
-    wage_10 = 0
-    wage_11 = 0
-    wage_12 = 0
-
-    if e00100<=0:
-        wage_1 = e00200 * s006
-    elif e00100 <= 10000:
-        wage_2 = e00200 * s006
-    elif e00100 <= 20000:
-        wage_3 = e00200 * s006
-    elif e00100 <= 30000:
-        wage_4 = e00200 * s006
-    elif e00100 <= 40000:
-        wage_5 = e00200 * s006
-    elif e00100 <= 50000:
-        wage_6 = e00200 * s006
-    elif e00100 <= 75000:
-        wage_7 = e00200 * s006
-    elif e00100 <= 100000:
-        wage_8 = e00200 * s006
-    elif e00100 <= 200000:
-        wage_9 = e00200 * s006
-    elif e00100 <= 500000:
-        wage_10 = e00200 * s006
-    elif e00100 <= 1000000:
-        wage_11 = e00200 * s006
-    else:
-        wage_12 = e00200 * s006
-        
-    
-    return (single_return, joint_return, hh_return, return_w_SS,
-            dependent_exempt_num, interest, dividend,
-            biz_income,biz_loss, cap_gain, annuity_pension,
-            sch_e_income, sch_e_loss, ss_income, unemployment_comp,
-            wage_1, wage_2, wage_3, wage_4, wage_5, wage_6,
-            wage_7, wage_8, wage_9, wage_10, wage_11, wage_12)   
-    
-        
-
-        
-        
-        
 
 
 
