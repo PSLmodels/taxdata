a6fd9a8d8c43b0bf22d7eb3f43de06a65e36e96d
Amy-Xu <amy.xu@aei.org>
2015-10-16 17:19:35 -0400

    update with current routine & description

--- Stage II/Single_Year_Stage_II.py
+++ Stage II/Single_Year_Stage_II.py
@@ -1,7 +1,7 @@
 
 import numpy as np
 import pandas as pd
-from pandas import DataFrame
+from pandas import DataFrame as df
 from cylp.cy import CyClpSimplex
 from cylp.py.modeling.CyLPModel import CyLPArray, CyLPModel
 
@@ -19,17 +19,17 @@ def Single_Year_Stage_II(puf, Stage_I_factors, Stage_II_targets, year, tol):
 
 
 
-    single_return = np.where(puf.mars==1, s006, 0)
-    joint_return = np.where((puf.mars==2)|(puf.mars==3), s006, 0)
-    hh_return = np.where(puf.mars==4,s006,0)
-    return_w_SS = np.where(puf.e02400>0,s006,0)
+    single_return = np.where((puf.mars==1) & (puf.filer==1), s006, 0)
+    joint_return = np.where(((puf.mars==2)|(puf.mars==3)) & (puf.filer==1), s006, 0)
+    hh_return = np.where((puf.mars==4) & (puf.filer==1),s006,0)
+    return_w_SS = np.where((puf.e02400>0) & (puf.filer==1),s006,0)
 
     dependent_exempt_num = (puf.xocah+puf.xocawh+puf.xoodep+puf.xopar)*s006
     interest = puf.e00300*s006
     dividend = puf.e00600*s006
     biz_income = np.where(puf.e00900>0, puf.e00900, 0)*s006
     biz_loss = np.where(puf.e00900<0, -puf.e00900, 0)*s006
-    cap_gain = np.where(puf.e01000>0, puf.e01000, 0)*s006
+    cap_gain = np.where((puf.p23250+puf.p22250)>0, (puf.p23250+puf.p22250), 0)*s006
     annuity_pension = puf.e01700*s006
     sch_e_income = np.where(puf.e02000>0, puf.e02000, 0)*s006
     sch_e_loss = np.where(puf.e02000<0, -puf.e02000, 0)*s006
@@ -73,66 +73,65 @@ def Single_Year_Stage_II(puf, Stage_I_factors, Stage_II_targets, year, tol):
 
     b = []
 
-    Single = Stage_II_targets[year]['Single']-single_return.sum()
-    Joint = Stage_II_targets[year]['Joint']-joint_return.sum()
-    HH = Stage_II_targets[year]['HH']-hh_return.sum()
-    SS_return = Stage_II_targets[year]['SS_return']-return_w_SS.sum()
-    Dep_return = Stage_II_targets[year]['Dep_return'] -  dependent_exempt_num.sum()
+    b.append(Stage_II_targets[year]['Single Returns']-single_return.sum())
+    b.append(Stage_II_targets[year]['Joint Returns']-joint_return.sum())
+    b.append(Stage_II_targets[year]['Head of Household Returns']-hh_return.sum())
+    b.append(Stage_II_targets[year]['Number of Returns w/ Gross Security Income']-return_w_SS.sum())
+
+    b.append(Stage_II_targets[year]['Number of Dependent Exemptions'] -  dependent_exempt_num.sum())
 
     AINTS = Stage_I_factors[year]["AINTS"]
-    INTEREST = Stage_II_targets[year]['INTS']*APOPN/AINTS*1000-interest.sum()
+    INTEREST = Stage_II_targets[year]['Taxable Interest Income']*APOPN/AINTS*1000-interest.sum()
 
     ADIVS = Stage_I_factors[year]["ADIVS"]
-    DIVIDEND = Stage_II_targets[year]['DIVS']*APOPN/ADIVS*1000 - dividend.sum()
+    DIVIDEND = Stage_II_targets[year]['Ordinary Dividends']*APOPN/ADIVS*1000 - dividend.sum()
 
     ASCHCI = Stage_I_factors[year]["ASCHCI"]
-    BIZ_INCOME = Stage_II_targets[year]['SCHCI']*APOPN/ASCHCI*1000 - biz_income.sum()
+    BIZ_INCOME = Stage_II_targets[year]['Business Income (Schedule C)']*APOPN/ASCHCI*1000 - biz_income.sum()
 
 
     ASCHCL = Stage_I_factors[year]["ASCHCL"]
-    BIZ_LOSS = Stage_II_targets[year]['SCHCL']*APOPN/ASCHCL*1000 - biz_loss.sum()
+    BIZ_LOSS = Stage_II_targets[year]['Business Loss (Schedule C)']*APOPN/ASCHCL*1000 - biz_loss.sum()
 
     ACGNS = Stage_I_factors[year]["ACGNS"]
-    CAP_GAIN = Stage_II_targets[year]['CGNS']*APOPN/ACGNS*1000 - cap_gain.sum()
+    CAP_GAIN = Stage_II_targets[year]['Net Capital Gains in AGI']*APOPN/ACGNS*1000 - cap_gain.sum()
 
     ATXPY = Stage_I_factors[year]["ATXPY"]
-    ANNUITY_PENSION = Stage_II_targets[year]['Pension']*APOPN/ATXPY*1000 - annuity_pension.sum()
+    ANNUITY_PENSION = Stage_II_targets[year]['Taxable Pensions and Annuities']*APOPN/ATXPY*1000 - annuity_pension.sum()
 
     ASCHEI = Stage_I_factors[year]["ASCHEI"]
-    SCH_E_INCOME = Stage_II_targets[year]["SCHEI"]*APOPN/ASCHEI*1000 - sch_e_income.sum()
+    SCH_E_INCOME = Stage_II_targets[year]["Supplemental Income (Schedule E)"]*APOPN/ASCHEI*1000 - sch_e_income.sum()
 
     ASCHEL = Stage_I_factors[year]["ASCHEL"]
-    SCH_E_LOSS = Stage_II_targets[year]["SCHEL"]*APOPN/ASCHEL*1000 - sch_e_loss.sum()
+    SCH_E_LOSS = Stage_II_targets[year]["Supplemental Loss (Schedule E)"]*APOPN/ASCHEL*1000 - sch_e_loss.sum()
 
     ASOCSEC = Stage_I_factors[year]["ASOCSEC"]
     APOPSNR = Stage_I_factors[year]["APOPSNR"]
-    SS_INCOME = Stage_II_targets[year]["SS"]*APOPSNR/ASOCSEC*1000 - ss_income.sum()
+    SS_INCOME = Stage_II_targets[year]["Gross Social Security Income"]*APOPSNR/ASOCSEC*1000 - ss_income.sum()
 
     AUCOMP = Stage_I_factors[year]["AUCOMP"]
-    UNEMPLOYMENT_COMP = Stage_II_targets[year]["UCOMP"]*APOPN/AUCOMP*1000 - unemployment_comp.sum()
+    UNEMPLOYMENT_COMP = Stage_II_targets[year]["Unemployment Compensation"]*APOPN/AUCOMP*1000 - unemployment_comp.sum()
 
     AWAGE = Stage_I_factors[year]["AWAGE"]
-    WAGE_1 = Stage_II_targets[year]["WAGE_1"]*APOPN/AWAGE*1000 - wage_1.sum()
-    WAGE_2 = Stage_II_targets[year]["WAGE_2"]*APOPN/AWAGE*1000 - wage_2.sum()
-    WAGE_3 = Stage_II_targets[year]["WAGE_3"]*APOPN/AWAGE*1000 - wage_3.sum()
-    WAGE_4 = Stage_II_targets[year]["WAGE_4"]*APOPN/AWAGE*1000 - wage_4.sum()
-    WAGE_5 = Stage_II_targets[year]["WAGE_5"]*APOPN/AWAGE*1000 - wage_5.sum()
-    WAGE_6 = Stage_II_targets[year]["WAGE_6"]*APOPN/AWAGE*1000 - wage_6.sum()
-    WAGE_7 = Stage_II_targets[year]["WAGE_7"]*APOPN/AWAGE*1000 - wage_7.sum()
-    WAGE_8 = Stage_II_targets[year]["WAGE_8"]*APOPN/AWAGE*1000 - wage_8.sum()
-    WAGE_9 = Stage_II_targets[year]["WAGE_9"]*APOPN/AWAGE*1000 - wage_9.sum()
-    WAGE_10 = Stage_II_targets[year]["WAGE_10"]*APOPN/AWAGE*1000 - wage_10.sum()
-    WAGE_11 = Stage_II_targets[year]["WAGE_11"]*APOPN/AWAGE*1000 - wage_11.sum()
-    WAGE_12 = Stage_II_targets[year]["WAGE_12"]*APOPN/AWAGE*1000 - wage_12.sum()
+    WAGE_1 = Stage_II_targets[year]["Wages and Salaries: Zero or Less"]*APOPN/AWAGE*1000 - wage_1.sum()
+    WAGE_2 = Stage_II_targets[year]["Wages and Salaries: $1 Less Than $10,000"]*APOPN/AWAGE*1000 - wage_2.sum()
+    WAGE_3 = Stage_II_targets[year]["Wages and Salaries: $10,000 Less Than $20,000"]*APOPN/AWAGE*1000 - wage_3.sum()
+    WAGE_4 = Stage_II_targets[year]["Wages and Salaries: $20,000 Less Than $30,000"]*APOPN/AWAGE*1000 - wage_4.sum()
+    WAGE_5 = Stage_II_targets[year]["Wages and Salaries: $30,000 Less Than $40,000"]*APOPN/AWAGE*1000 - wage_5.sum()
+    WAGE_6 = Stage_II_targets[year]["Wages and Salaries: $40,000 Less Than $50,000"]*APOPN/AWAGE*1000 - wage_6.sum()
+    WAGE_7 = Stage_II_targets[year]["Wages and Salaries: $50,000 Less Than $75,000"]*APOPN/AWAGE*1000 - wage_7.sum()
+    WAGE_8 = Stage_II_targets[year]["Wages and Salaries: $75,000 Less Than $100,000"]*APOPN/AWAGE*1000 - wage_8.sum()
+    WAGE_9 = Stage_II_targets[year]["Wages and Salaries: $100,000 Less Than $200,000"]*APOPN/AWAGE*1000 - wage_9.sum()
+    WAGE_10 = Stage_II_targets[year]["Wages and Salaries: $200,000 Less Than $500,000"]*APOPN/AWAGE*1000 - wage_10.sum()
+    WAGE_11 = Stage_II_targets[year]["Wages and Salaries: $500,000 Less Than $1 Million"]*APOPN/AWAGE*1000 - wage_11.sum()
+    WAGE_12 = Stage_II_targets[year]["Wages and Salaries: $1 Million and Over"]*APOPN/AWAGE*1000 - wage_12.sum()
 
 
 
-    temp = [Single, Joint, HH, SS_return, Dep_return,
-            INTEREST,DIVIDEND, BIZ_INCOME, BIZ_LOSS, CAP_GAIN, ANNUITY_PENSION,
-            SCH_E_INCOME, SCH_E_LOSS, SS_INCOME, UNEMPLOYMENT_COMP,
-            WAGE_1,WAGE_2, WAGE_3, WAGE_4, WAGE_5, WAGE_6,
-            WAGE_7,WAGE_8, WAGE_9, WAGE_10, WAGE_11, WAGE_12]
-    b = list(temp)
+    temp = [INTEREST,DIVIDEND, BIZ_INCOME, BIZ_LOSS, CAP_GAIN, ANNUITY_PENSION, SCH_E_INCOME, SCH_E_LOSS, SS_INCOME, UNEMPLOYMENT_COMP,
+            WAGE_1,WAGE_2, WAGE_3,WAGE_4, WAGE_5, WAGE_6, WAGE_7,WAGE_8,WAGE_9, WAGE_10, WAGE_11, WAGE_12]
+    for m in temp:
+        b.append(m)
 
     targets = CyLPArray(b)
     print("Targets for year ", year, " is ", targets)
@@ -164,7 +163,7 @@ def Single_Year_Stage_II(puf, Stage_I_factors, Stage_II_targets, year, tol):
 
     print("DONE!!")
     z = np.empty([length])
-    z = (1+model.primalVariableSolution['r'] - model.primalVariableSolution['s'])*s006
+    z = (1+model.primalVariableSolution['r'] - model.primalVariableSolution['s'])*s006 * 100
     return z
 
 
