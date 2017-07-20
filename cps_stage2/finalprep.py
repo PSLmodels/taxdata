88f657e586b87eee489596860920d7e5700bf64d
andersonfrailey <anderson.frailey@aei.org>
2017-07-20 11:43:12 -0400

    multiply before round

--- cps_stage2/finalprep.py
+++ cps_stage2/finalprep.py
@@ -2,6 +2,6 @@ import pandas as pd
 
 
 weights = pd.read_csv('cps_weights_raw.csv.gz', compression='gzip')
+weights *= 100.
 weights = weights.round(0).astype('int64')
-weights *= 100
 weights.to_csv('cps_weights.csv.gz', index=False, compression='gzip')
