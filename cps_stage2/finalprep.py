fe45c82c3d4a432016962904ee0c188f9549d6e7
martinholmer <martin.holmer@gmail.com>
2017-07-20 16:07:52 -0400

    Add gzip -f option to eliminate interactive prompting

--- cps_stage2/finalprep.py
+++ cps_stage2/finalprep.py
@@ -6,4 +6,4 @@ weights = pd.read_csv('cps_weights_raw.csv.gz', compression='gzip')
 weights *= 100.
 weights = weights.round(0).astype('int64')
 weights.to_csv('cps_weights.csv', index=False)
-subprocess.check_call(['gzip', '-n', 'cps_weights.csv'])
+subprocess.check_call(['gzip', '-nf', 'cps_weights.csv'])
