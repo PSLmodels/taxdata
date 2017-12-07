bfb66730a004e52c021a3f1bd8872bf3a4fe17ab
Amy-Xu <amy.xu@aei.org>
2017-12-07 13:50:57 -0500

    modified for the updated benefit file

--- cps_stage3/test_cps_benefits.py
+++ cps_stage3/test_cps_benefits.py
@@ -4,20 +4,25 @@ import numpy as np
 from pandas.util.testing import assert_frame_equal
 
 programs = ['ss', 'ssi', 'medicaid', 'medicare', 'vb', 'snap']
+billion = 10e9
+million = 10e6
 
 def read_files():
     ''' import weights, benefit, and raw cps file'''
     
     # import from taxdata repo
+    # weights and wage are for 10-year and decile tables
     weights = pd.read_csv('../cps_stage2/cps_weights.csv.gz', compression='gzip')
-    cps_income = pd.read_csv('../cps_data/cps.csv.gz', compression='gzip')[['e00200', 's006']]
-    cps_benefit = pd.read_csv('cps_benefits.csv')
+    cps_income = pd.read_csv('../cps_data/cps.csv.gz',
+                             compression='gzip')[['e00200', 's006', 'RECID']]
+    # the benefit file that includes both benefits and recipients
+    cps_benefit = pd.read_csv('cps_benefits_extrap_full.csv.gz')
 
-    assert len(cps_benefit) == len(weights)
-    assert len(cps_income) == len(cps_benefit)
+    assert len(cps_income) == len(weights)
     
     # merge all essential variables
-    cps = cps_benefit.join(cps_income)
+    cps = cps_income.merge(cps_benefit, on='RECID', how='left')
+    cps.fillna(0, inplace=True)
     cps = cps.join(weights)
     
     # rename to facilitate for loops
@@ -52,14 +57,15 @@ def test_decile_dist():
         cps['dummy'] = np.where(cps[p_vars[i]]!=0, cps['WT2015'], 0)
         
         # calculate total benefits, participation (# tax units), and average per decile
-        bp = cps[[benefits_vars[i] + '_weighted', 'dummy']].groupby(cps.WT2015_decile, as_index=False).sum()/1000000
+        bp = cps[[benefits_vars[i] + '_weighted', 'dummy']].groupby(cps.WT2015_decile,
+                                                                    as_index=False).sum()/million
         bp['average'] = bp[benefits_vars[i] + '_weighted']/(bp['dummy'] + delta)
 
         # rename and save
         bp.columns = [programs[i]+'_benefits', programs[i]+'_taxunits', programs[i]+'_average']
         decile2015 = pd.concat([decile2015, bp], axis=1)
         
-        decile2015.to_csv('decile2015_new.csv', float_format='%.2f', index=False)
+        decile2015.to_csv('decile2015_new.csv', float_format='%.1f', index=False)
 
     decile_old = pd.read_csv('decile2015.csv')
     assert_frame_equal(decile2015.round(1), decile_old)
@@ -80,20 +86,20 @@ def test_aggregates():
         benefits_vars = [x + '_benefits_' + str(year) for x in programs]
         raw_benefits = cps.loc[:,benefits_vars]
         weighted_benefits = raw_benefits.multiply(cps['WT' + str(year)], axis='index')
-        benefit_total = pd.DataFrame(weighted_benefits.sum()/1000000000)
+        benefit_total = pd.DataFrame(weighted_benefits.sum()/billion)
         benefits[year] = benefit_total.values
 
         #participants
         p_vars = [x + '_recipients_'+ str(year) for x in programs]
         raw_participants = cps.loc[:, p_vars]
         weighted_par = raw_participants.multiply(cps['WT' + str(year)], axis='index')
-        participant_total = pd.DataFrame(weighted_par.sum()/1000000)
+        participant_total = pd.DataFrame(weighted_par.sum()/million)
         participants[year] = participant_total.values
 
         # tax units
         dummy = raw_participants.astype(bool)
         weighted_taxunits = dummy.multiply(cps['WT' + str(year)], axis='index')
-        taxunit_total = pd.DataFrame(weighted_taxunits.sum()/1000000)
+        taxunit_total = pd.DataFrame(weighted_taxunits.sum()/million)
         taxunits[year] = taxunit_total.values
 
     pd.options.display.float_format = '{:,.1f}'.format
@@ -131,8 +137,8 @@ def test_tabs():
         for year in range(2014, 2025): 
             program_tab[year] = p_tab(program+"_recipients_"+str(year))
             program_tab = pd.DataFrame(program_tab)
-        
-        tabs[program] = program_tab
+            program_tab.fillna(0, inplace=True)
+        tabs[program] = program_tab.astype(int)
 
     with open('tabs_new.txt', 'w') as file:
         for key, dfs in tabs.iteritems():
@@ -149,10 +155,11 @@ def test_tabs():
         start_row = (tabs_old.index[tabs_old['index']==program] + 1).values[0]
         end_row = start_row + unitmax
     
-        participation_new = tabs_old.loc[start_row: end_row]
-        participation_new = participation_new.reset_index().drop(['level_0'], axis=1)
+        participation_old = tabs_old.loc[start_row: end_row]
+        participation_old = participation_old.reset_index().drop(['level_0'], axis=1)
     
-        assert_frame_equal(participation_new.astype(int), tabs[program].reset_index(),
+        assert_frame_equal(participation_old.astype(float),
+                           tabs[program].reset_index().astype(float),
                            check_column_type=False, check_index_type=False)
 
 
