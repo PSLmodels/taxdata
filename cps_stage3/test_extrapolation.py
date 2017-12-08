50ce2cf84851c47abefbc13b0d2a118c2446933c
Henry Doupe <hdoupe@yahoo.com>
2017-12-08 15:08:02 -0500

    Add more rigorous tests to check adding and removing of particpants

--- cps_stage3/test_extrapolation.py
+++ cps_stage3/test_extrapolation.py
@@ -45,24 +45,55 @@ def test_add_participants():
     benefit_names = ["medicare"]
     ben = Benefits(benefit_names=benefit_names)
 
-    has_benefits = ben.medicare_participation[ben.medicare_participation.sum(axis=1) > 0]
+    shape = ben.medicare_participation.shape
+    recid_ravel = Benefits._repeating_ravel(shape,
+                                            ben.medicare_participation.index.values)
+
+    ravel_df = Benefits._ravel_data(
+        WT=ben.WT.loc[:, 'WT2015'].values * 0.01,
+        I=ben.medicare_participation,
+        benefits=ben.medicare_benefits,
+        prob=ben.medicare_prob
+    )
+    ravel_df['recid'] = recid_ravel
+
+    target = ben.medicare_participant_targets[2015]
+
+    candidates = ravel_df.loc[ravel_df.I == 0, ].copy()
+    assert candidates.I.sum() == 0
+    candidates.loc[:, "I"] = np.ones(len(candidates))
+    candidates.loc[:, "I_wt"] = candidates.I * candidates.WT
+
+    noncandidates = ravel_df.loc[ravel_df.I > 0, ].copy()
+    assert noncandidates.I.sum() == len(noncandidates)
+
+    candidates.sort_values("prob", inplace=True, ascending=False)
+    noncan_part = noncandidates["I_wt"].sum()
+    candidates["cum_participants"] = candidates["I_wt"].cumsum() + noncan_part
+    candidates["_diff"] = candidates["cum_participants"] - target
+
+    keeps_benefits = candidates.loc[candidates._diff <= 0, ["recid", "j", "prob", "_diff", "cum_participants"]]
+    loses_benefits = candidates.loc[candidates._diff > 0, ["recid", "j", "prob", "_diff", "cum_participants"]]
+
+    for df in [keeps_benefits, loses_benefits]:
+        for _, row in df.iterrows():
+            assert np.allclose(
+                ben.medicare_participation.iloc[int(row['recid'])][row['j']],
+                np.zeros(1)
+            )
 
     ben.increment_year()
 
-    after = ben.medicare_participation[ben.medicare_participation.sum(axis=1) > 0]
-    assert len(after) > len(has_benefits)
-    overlap = has_benefits.join(after, how="inner", rsuffix="rem")
-
-    assert (overlap.index == has_benefits.index).all()
-
-    no_benefits_overlap = overlap[[c for c in overlap if not c.endswith('rem')]]
-    no_benefits_overlap.columns = has_benefits.columns
-
-    pd.testing.assert_frame_equal(no_benefits_overlap, has_benefits,
-                                  check_dtype=False, check_column_type=False)
-
-    no_benefits_ix = set(has_benefits.index)
-    assert len(no_benefits_ix - set(after.index)) == 0
+    for _, row in keeps_benefits.iterrows():
+        assert np.allclose(
+            ben.medicare_participation.iloc[int(row['recid'])][row['j']],
+            np.ones(1)
+        )
+    for _, row in loses_benefits.iterrows():
+        assert np.allclose(
+            ben.medicare_participation.iloc[int(row['recid'])][row['j']],
+            np.zeros(1)
+        )
 
 
 def test_remove_participants():
@@ -74,24 +105,56 @@ def test_remove_participants():
     benefit_names = ["snap"]
     ben = Benefits(benefit_names=benefit_names)
 
-    no_benefits = ben.snap_participation[ben.snap_participation.sum(axis=1) == 0]
+    shape = ben.snap_participation.shape
+    recid_ravel = Benefits._repeating_ravel(shape,
+                                            ben.snap_participation.index.values)
+
+    ravel_df = Benefits._ravel_data(
+        WT=ben.WT.loc[:, 'WT2015'].values * 0.01,
+        I=ben.snap_participation,
+        benefits=ben.snap_benefits,
+        prob=ben.snap_prob
+    )
+    ravel_df['recid'] = recid_ravel
+
+    target = ben.snap_participant_targets[2015]
+
+    candidates = ravel_df.loc[ravel_df.I > 0, ].copy()
+    assert candidates.I.sum() == len(candidates)
+    noncandidates = ravel_df.loc[ravel_df.I == 0, ].copy()
+    assert noncandidates.I.sum() == 0
+
+
+    candidates.sort_values("prob", inplace=True, ascending=False)
+    noncan_part = noncandidates["I_wt"].sum()
+    candidates["cum_participants"] = candidates["I_wt"].cumsum() + noncan_part
+    candidates["_diff"] = candidates["cum_participants"] - target
+
+    keeps_benefits = candidates.loc[candidates._diff <= 0, ["recid", "j", "prob", "_diff", "cum_participants"]]
+    loses_benefits = candidates.loc[candidates._diff > 0, ["recid", "j", "prob", "_diff", "cum_participants"]]
+
+    # last_keep = keeps_benefits.nsmallest(len(keeps_benefits), "prob")
+    # first_lose = loses_benefits.nlargest(len(loses_benefits), "prob")
+
+    for df in [keeps_benefits, loses_benefits]:
+        for _, row in df.iterrows():
+            assert np.allclose(
+                ben.snap_participation.iloc[int(row['recid'])][row['j']],
+                np.ones(1)
+            )
 
     ben.increment_year()
 
-    after = ben.snap_participation[ben.snap_participation.sum(axis=1) == 0]
-    assert len(after) > len(no_benefits)
-    overlap = no_benefits.join(after, how="inner", rsuffix="rem")
-
-    assert (overlap.index == no_benefits.index).all()
-
-    no_benefits_overlap = overlap[[c for c in overlap if not c.endswith('rem')]]
-    no_benefits_overlap.columns = no_benefits.columns
-
-    pd.testing.assert_frame_equal(no_benefits_overlap, no_benefits,
-                                  check_dtype=False, check_column_type=False)
-
-    no_benefits_ix = set(no_benefits.index)
-    assert len(no_benefits_ix - set(after.index)) == 0
+    for _, row in keeps_benefits.iterrows():
+        assert np.allclose(
+            ben.snap_participation.iloc[int(row['recid'])][row['j']],
+            np.ones(1)
+        )
+    for _, row in loses_benefits.iterrows():
+        assert np.allclose(
+            ben.snap_participation.iloc[int(row['recid'])][row['j']],
+            np.zeros(1)
+        )
 
 
 def test_add_particpants_small():
