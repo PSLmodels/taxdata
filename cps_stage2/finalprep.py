a72e32f094800274b44a3bb7ba1431a49c471bef
andersonfrailey <anderson.frailey@aei.org>
2018-04-24 10:40:02 -0400

    add 2014 column to weights

--- cps_stage2/finalprep.py
+++ cps_stage2/finalprep.py
@@ -6,5 +6,10 @@ weights = pd.read_csv('cps_weights_raw.csv.gz', compression='gzip')
 weights = weights.drop('SEQUENCE', axis=1)
 weights *= 100.
 weights = weights.round(0).astype('int64')
-weights.to_csv('cps_weights.csv', index=False)
+# add weights from CPS as 2014 value
+cps = pd.read_csv('../cps_data/cps.csv.gz')
+cps.s006 *= 100
+df = pd.concat([cps.s006, weights], axis=1)
+df = df.rename({'s006': 'WT2014'}, axis=1)
+df.to_csv('cps_weights.csv', index=False)
 subprocess.check_call(['gzip', '-nf', 'cps_weights.csv'])
