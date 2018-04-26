670cfdf12ebfd365eca119c04a4169a962d4409b
andersonfrailey <anderson.frailey@aei.org>
2018-04-26 11:17:18 -0400

    move weight multiplication

--- cps_stage2/finalprep.py
+++ cps_stage2/finalprep.py
@@ -8,7 +8,6 @@ weights *= 100.
 weights = weights.round(0).astype('int64')
 # add weights from CPS as 2014 value
 cps = pd.read_csv('../cps_data/cps.csv.gz')
-cps.s006 *= 100
 df = pd.concat([cps.s006, weights], axis=1)
 df = df.rename({'s006': 'WT2014'}, axis=1)
 df.to_csv('cps_weights.csv', index=False)
