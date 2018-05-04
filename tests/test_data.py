ade7efa92e7d3b099d9df05e9e568463a1af0b02
andersonfrailey <anderson.frailey@aei.org>
2018-05-04 14:57:10 -0400

    additional data tests

--- tests/test_data.py
+++ tests/test_data.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 
-def min_max(data, meta, data_name):
+def min_max(data, meta, dataname):
     """
     Test for ensuring all variables are within their logical minimum and
     maximum
@@ -11,19 +11,41 @@ def min_max(data, meta, data_name):
         min_value = meta[var]['range']['min']
         max_value = meta[var]['range']['max']
         in_data = True
-        if var == 'e19800':
-            print('here')
-        if data_name not in availability:
+        if dataname not in availability:
             in_data = False
         if in_data:
-            m = f'{data_name}-{var} contains values less than minimum value'
-            if var == 'e19800':
-                print(data_name)
+            m = '{}-{} contains values less than min value'.format(dataname,
+                                                                   var)
             assert np.all(data[var] >= min_value), m
-            m = f'{data_name}-{var} contains values greater than maximum value'
+            m = '{}-{} contains values greater than max value'.format(dataname,
+                                                                      var)
             assert np.all(data[var] <= max_value), m
 
 
-def test_min_max(cps, puf, metadata):
-    min_max(cps, metadata, 'CPS')
-    min_max(puf, metadata, 'PUF')
+def test_data(cps, puf, metadata):
+    min_max(cps, metadata, 'cps')
+    min_max(puf, metadata, 'puf')
+    relationships(cps)
+    relationships(puf)
+
+
+def relationships(data):
+    """
+    Test the relative relationships between variables
+    """
+    eq_str = '{} not equal to {}'
+    less_than_str = '{} not less than or equal to {}'
+    tol = 0.020001
+
+    eq_vars = [('e00200', ['e00200p', 'e00200s']),
+               ('e00900', ['e00900p', 'e00900s']),
+               ('e02100', ['e02100p', 'e02100s'])]
+    for lhs, rhs in eq_vars:
+        if not np.allclose(data[lhs], data[rhs].sum(axis=1), atol=tol):
+            raise ValueError(eq_str.format(lhs, rhs))
+
+    less_than_vars = [('XTOT', ['nu18', 'n1820', 'n21']),
+                      ('n24', ['nu18'])]
+    for lhs, rhs in less_than_vars:
+        m = less_than_str.format(lhs, 'sum of {}'.format(rhs))
+        assert np.all(data[lhs] <= data[rhs].sum(axis=1)), m
