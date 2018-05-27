b97213787f2a6f47c0a5133c929e7b08ec8dbdc1
martinholmer <martin.holmer@gmail.com>
2018-05-27 18:37:13 -0400

    Add weights tests

--- tests/test_growfactors.py
+++ tests/test_growfactors.py
@@ -1,36 +1,14 @@
 import numpy as np
 
 
-def min_max(data, meta, dataname):
-    """
-    Test for ensuring all variables are within their logical minimum and
-    maximum
-    """
-    for var in meta.keys():
-        availability = meta[var]['availability']
-        min_value = meta[var]['range']['min']
-        max_value = meta[var]['range']['max']
-        in_data = True
-        if dataname not in availability:
-            in_data = False
-        if in_data:
-            m = '{}-{} contains values less than min value'.format(dataname,
-                                                                   var)
-            assert np.all(data[var] >= min_value), m
-            m = '{}-{} contains values greater than max value'.format(dataname,
-                                                                      var)
-            assert np.all(data[var] <= max_value), m
-
-
 def test_growfactor_start_year(growfactors):
-    first_year = growfactors['YEAR'].min()
+    first_growfactors_year = growfactors.index.min()
     first_taxcalc_policy_year = 2013
-    assert first_year <= first_taxcalc_policy_year
+    assert first_growfactors_year <= first_taxcalc_policy_year
 
 
 def test_growfactor_values(growfactors):
-    first_year = growfactors['YEAR'].min()
-    growfactors.set_index('YEAR', inplace=True)
+    first_year = growfactors.index.min()
     for fname in growfactors:
         if fname != 'YEAR':
             assert growfactors[fname][first_year] == 1.0
---
+++ tests/test_weights.py
@@ -0,0 +1,36 @@
+import pytest
+import numpy as np
+
+
+@pytest.mark.parametrize('kind', ['puf', 'cps'])
+def test_weights(kind, puf_weights, cps_weights, growfactors):
+    # test range of years in weights file
+    first_growfactors_year = growfactors.index.min()
+    last_growfactors_year = growfactors.index.max()
+    if kind == 'puf':
+        weights = puf_weights
+        first_weights_year = 2011  # change value when using newer PUF data
+        last_weights_year = last_growfactors_year
+    elif kind == 'cps':
+        weights = cps_weights
+        first_weights_year = 2014  # change value when using newer CPS data
+        last_weights_year = last_growfactors_year
+    else:
+       raise ValueError('illegal weights kind = {}'.format(kind))
+    sorted_weights_columns = sorted([col for col in weights])
+    first_weights_column = sorted_weights_columns[0]
+    last_weights_column = sorted_weights_columns[-1]
+    assert first_weights_column == 'WT{}'.format(first_weights_year)
+    assert last_weights_column == 'WT{}'.format(last_weights_year)
+    # test range of weight values for each year
+    min_weight = 0  # must be non-negative,
+    max_weight = 1800000  # but can be large
+    for col in weights:
+       if weights[col].min() < min_weight:
+           msg = '{} weights[{}].min()={} < {}'
+           raise ValueError(msg.format(kind, col,
+                                       weights[col].min(), min_weight))
+       if weights[col].max() > max_weight:
+           msg = '{} weights[{}].max()={} > {}'
+           raise ValueError(msg.format(kind, col,
+                                       weights[col].max(), max_weight))
