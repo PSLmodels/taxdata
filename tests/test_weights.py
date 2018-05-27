0b2f815cb3007d96242844a086cf4304b53cdfdc
martinholmer <martin.holmer@gmail.com>
2018-05-27 19:03:04 -0400

    Revise test_weights.py code

--- tests/test_weights.py
+++ tests/test_weights.py
@@ -3,20 +3,20 @@ import numpy as np
 
 
 @pytest.mark.parametrize('kind', ['puf', 'cps'])
-def test_weights(kind, puf_weights, cps_weights, growfactors, puf, cps):
+def test_weights(kind, puf_weights, cps_weights, growfactors):
     # test range of years in weights file
     first_growfactors_year = growfactors.index.min()
     last_growfactors_year = growfactors.index.max()
     if kind == 'puf':
         weights = puf_weights
         first_weights_year = 2011  # change value when using newer PUF data
+        expected_num_records = 239002  # change value when using newer PUF data
         last_weights_year = last_growfactors_year
-        data = puf
     elif kind == 'cps':
         weights = cps_weights
         first_weights_year = 2014  # change value when using newer CPS data
+        expected_num_records = 456465  # change value when using newer CPS data
         last_weights_year = last_growfactors_year
-        data = cps
     else:
        raise ValueError('illegal kind = {}'.format(kind))
     sorted_weights_columns = sorted([col for col in weights])
@@ -25,7 +25,7 @@ def test_weights(kind, puf_weights, cps_weights, growfactors, puf, cps):
     assert first_weights_column == 'WT{}'.format(first_weights_year)
     assert last_weights_column == 'WT{}'.format(last_weights_year)
     # test number of rows in weights file
-    assert weights.shape[0] == data.shape[0]
+    assert weights.shape[0] == expected_num_records
     # test range of weight values for each year
     min_weight = 0  # weight must be non-negative,
     max_weight = 1800000  # but can be quite large
