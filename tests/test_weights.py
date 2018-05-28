fa41f5fe5db4e214b4708d5b1c82b6dfaed51892
martinholmer <martin.holmer@gmail.com>
2018-05-28 12:12:45 -0400

    Add weights tests

--- tests/test_weights.py
+++ tests/test_weights.py
@@ -2,39 +2,64 @@ import pytest
 import numpy as np
 
 
-@pytest.mark.parametrize('kind', ['puf', 'cps'])
-def test_weights(kind, puf_weights, cps_weights, growfactors):
-    # test range of years in weights file
-    first_growfactors_year = growfactors.index.min()
-    last_growfactors_year = growfactors.index.max()
-    if kind == 'puf':
-        weights = puf_weights
-        first_weights_year = 2011  # change value when using newer PUF data
-        expected_num_records = 239002  # change value when using newer PUF data
-        last_weights_year = last_growfactors_year
-    elif kind == 'cps':
+@pytest.mark.parametrize('kind', ['cps', 'puf'])
+def test_weights(kind, cps_weights, puf_weights,
+                 growfactors, cps_count, puf_count,
+                 cps_start_year, puf_start_year):
+    # specify weights dataframe and related parameters
+    if kind == 'cps':
         weights = cps_weights
-        first_weights_year = 2014  # change value when using newer CPS data
-        expected_num_records = 456465  # change value when using newer CPS data
-        last_weights_year = last_growfactors_year
+        count = cps_count
+        first_year = cps_start_year
+    elif kind == 'puf':
+        weights = puf_weights
+        count = puf_count
+        first_year = puf_start_year
     else:
-       raise ValueError('illegal kind = {}'.format(kind))
+        raise ValueError('illegal kind={}'.format(kind))
+    # test count of records in weights
+    if weights.shape[0] != count:
+        msg = '{} weights.shape[0]={} < {}'
+        raise ValueError(msg.format(kind, weights.shape[0], count))
+    # test range of years in weights file
     sorted_weights_columns = sorted([col for col in weights])
     first_weights_column = sorted_weights_columns[0]
+    assert first_weights_column == 'WT{}'.format(first_year)
     last_weights_column = sorted_weights_columns[-1]
-    assert first_weights_column == 'WT{}'.format(first_weights_year)
-    assert last_weights_column == 'WT{}'.format(last_weights_year)
-    # test number of rows in weights file
-    assert weights.shape[0] == expected_num_records
+    assert last_weights_column == 'WT{}'.format(growfactors.index.max())
     # test range of weight values for each year
     min_weight = 0  # weight must be non-negative,
-    max_weight = 1800000  # but can be quite large
+    max_weight = 2000000  # but can be quite large
     for col in weights:
-       if weights[col].min() < min_weight:
-           msg = '{} weights[{}].min()={} < {}'
-           raise ValueError(msg.format(kind, col,
-                                       weights[col].min(), min_weight))
-       if weights[col].max() > max_weight:
-           msg = '{} weights[{}].max()={} > {}'
-           raise ValueError(msg.format(kind, col,
-                                       weights[col].max(), max_weight))
+        if weights[col].min() < min_weight:
+            msg = '{} weights[{}].min()={} < {}'
+            raise ValueError(msg.format(kind, col,
+                                        weights[col].min(), min_weight))
+        if weights[col].max() > max_weight:
+            msg = '{} weights[{}].max()={} > {}'
+            raise ValueError(msg.format(kind, col,
+                                        weights[col].max(), max_weight))
+    # test sum of weights (in millions) for each year
+    min_weight_sum = 150
+    max_weight_sum = 200
+    for col in weights:
+        weight_sum = weights[col].sum() * 1e-2 * 1e-6  # in millions
+        if weight_sum < min_weight_sum:
+            msg = '{} weights[{}].sum()={:.1f} < {:.1f}'
+            raise ValueError(msg.format(kind, col, weight_sum, max_weight_sum))
+        if weight_sum > max_weight_sum:
+            msg = '{} weights[{}].max()={:.1f} > {:.1f}'
+            raise ValueError(msg.format(kind, col, weight_sum, max_weight_sum))
+    # test that there are no weights records with a zero weight in every year
+    num_nonzero_years = np.count_nonzero(weights, axis=1)
+    num_total_years = num_nonzero_years.copy()
+    num_total_years.fill(growfactors.index.max() - first_year + 1)
+    num_allzero_years = num_total_years - num_nonzero_years
+    if np.any(num_allzero_years):
+        num_allzero_records = np.count_nonzero(num_allzero_years)
+        txt = 'number {} records with zero weight in every year = {}'
+        msg = txt.format(kind, num_allzero_records)
+        if kind == 'puf' and num_allzero_records <= 1:
+            print 'WARNING: ' + msg
+        else:
+            raise ValueError(msg)
