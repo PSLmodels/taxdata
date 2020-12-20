fb843e48982a21d2d9ab093ac320935e58cbb3ad
andersonfrailey <andersonfrailey@gmail.com>
2020-12-20 13:56:36 -0600

    modify automated reports

--- history/report.py
+++ history/report.py
@@ -15,6 +15,7 @@ from report_utils import (
     compare_vars,
     cbo_bar_chart,
     agg_liability_table,
+    compare_calcs,
 )
 from pathlib import Path
 from datetime import datetime
@@ -32,7 +33,7 @@ SOI_URL = "https://raw.githubusercontent.com/PSLmodels/taxdata/master/puf_stage1
 META_URL = "https://raw.githubusercontent.com/PSLmodels/taxdata/master/tests/records_metadata.json"
 GROW_FACTORS_URL = "https://raw.githubusercontent.com/PSLmodels/taxdata/master/puf_stage1/growfactors.csv"
 
-PUF_PATH = Path(CUR_PATH, "..", "puf_data", "puf.csv")
+PUF_PATH = Path(CUR_PATH, "..", "data", "puf.csv")
 PUF_AVAILABLE = PUF_PATH.exists()
 
 TEMPLATE_PATH = Path(CUR_PATH, "report_template.md")
@@ -79,8 +80,16 @@ def report():
         default="",
         type=str,
     )
+    parser.add_argument(
+        "--basepuf",
+        help=(
+            "File path to the previous puf.csv file. Use when the proposed "
+            "changes affect puf.csv"
+        ),
+    )
     args = parser.parse_args()
     desc = args.desc
+    base_puf_path = args.basepuf
     if desc:
         desc = Path(args.desc).open("r").read()
     plot_paths = []
@@ -108,7 +117,7 @@ def report():
     if new_cbo.equals(cur_cbo):
         cbo_projections.append("No changes to CBO projections.")
     else:
-        # we"re only going to include the final ten years in our bar chart
+        # we're only going to include the final ten years in our bar chart
         keep_years = [str(year) for year in range(first_year, last_year + 1)]
         cur_cbo = cur_cbo.filter(keep_years, axis=1).transpose().reset_index()
         cur_cbo["Projections"] = "Current"
@@ -173,18 +182,12 @@ def report():
     template_args["growth_rate_projections"] = growth_rate_projections
 
     # compare tax calculator projections
-    calcs = []
-    calc_labels = []
     # baseline CPS calculator
     base_cps = tc.Calculator(records=tc.Records.cps_constructor(), policy=tc.Policy())
     base_cps.advance_to_year(first_year)
     base_cps.calc_all()
-    calcs.append(base_cps)
-    calc_labels.append("Current CPS")
     # updated CPS calculator
-    cps = pd.read_csv(
-        Path(CUR_PATH, "..", "cps_data", "pycps", "cps.csv.gz"), index_col=None
-    )
+    cps = pd.read_csv(Path(CUR_PATH, "..", "data", "cps.csv.gz"), index_col=None)
     cps_weights = pd.read_csv(
         Path(CUR_PATH, "..", "cps_stage2", "cps_weights.csv.gz"), index_col=None
     )
@@ -196,79 +199,114 @@ def report():
     )
     new_cps.advance_to_year(first_year)
     new_cps.calc_all()
-    calcs.append(new_cps)
-    calc_labels.append("New CPS")
-    # distribution plots
-    dist_vars = [
-        ("c00100", "AGI Distribution"),
-        ("combined", "Tax Liability Distribution"),
-    ]
-    dist_plots = []
-    for var, title in dist_vars:
-        plot = distplot(calcs, calc_labels, var, title=title)
-        img_path = Path(CUR_PATH, f"{var}_dist.png")
-        plot.save(str(img_path))
-        plot_paths.append(img_path)
-        dist_plots.append(f"![]({str(img_path)})" + "{.center}")
-    template_args["dist_plots"] = dist_plots
-
-    # aggregate totals
-    aggs = defaultdict(list)
-    var_list = ["payrolltax", "iitax", "combined", "standard", "c04470"]
-    for year in range(first_year, tc.Policy.LAST_BUDGET_YEAR + 1):
-        base_aggs = run_calc(base_cps, year, var_list)
-        new_aggs = run_calc(new_cps, year, var_list)
-        aggs["Tax Liability"].append(base_aggs["payrolltax"])
-        aggs["Tax"].append("Current Payroll")
-        aggs["Year"].append(year)
-        aggs["Tax Liability"].append(new_aggs["payrolltax"])
-        aggs["Tax"].append("New Payroll")
-        aggs["Year"].append(year)
-        aggs["Tax Liability"].append(base_aggs["iitax"])
-        aggs["Tax"].append("Current Income")
-        aggs["Year"].append(year)
-        aggs["Tax Liability"].append(new_aggs["iitax"])
-        aggs["Tax"].append("New Income")
-        aggs["Year"].append(year)
-        aggs["Tax Liability"].append(base_aggs["combined"])
-        aggs["Tax"].append("Current Combined")
-        aggs["Year"].append(year)
-        aggs["Tax Liability"].append(new_aggs["combined"])
-        aggs["Tax"].append("New Combined")
-        aggs["Year"].append(year)
-    agg_df = pd.DataFrame(aggs)
-
-    title = "Aggregate Tax Liability by Year"
-    agg_chart = (
-        alt.Chart(agg_df, title=title)
-        .mark_line()
-        .encode(
-            x=alt.X(
-                "Year:O",
-                axis=alt.Axis(labelAngle=0, titleFontSize=20, labelFontSize=15),
-            ),
-            y=alt.Y(
-                "Tax Liability",
-                title="Tax Liability (Billions)",
-                axis=alt.Axis(titleFontSize=20, labelFontSize=15),
-            ),
-            color=alt.Color(
-                "Tax",
-                legend=alt.Legend(symbolSize=150, labelFontSize=15, titleFontSize=20),
-            ),
-        )
-        .properties(width=800, height=350)
-        .configure_title(fontSize=24)
+    template_args, plot_paths = compare_calcs(
+        base_cps, new_cps, "cps", template_args, plot_paths
     )
-    img_path = Path(CUR_PATH, "agg_plot.png")
-    agg_chart.save(str(img_path))
-    plot_paths.append(img_path)
-    template_args["agg_plot"] = f"![]({str(img_path)})" + "{.center}"
 
-    # create tax liability tables
-    template_args["combined_table"] = agg_liability_table(agg_df, "Combined")
-    template_args["payroll_table"] = agg_liability_table(agg_df, "Payroll")
-    template_args["income_table"] = agg_liability_table(agg_df, "Income")
+    # PUF comparison
+    if base_puf_path and PUF_AVAILABLE:
+        template_args["puf_msg"] = None
+        # base puf calculator
+        base_puf = tc.Calculator(
+            records=tc.Records(data=base_puf_path), policy=tc.Policy()
+        )
+        base_puf.advance_to_year(first_year)
+        base_puf.calc_all()
+        # updated puf calculator
+        puf_weights = pd.read_csv(
+            Path(CUR_PATH, "..", "puf_stage2", "puf_weights.csv.gz"), index_col=None
+        )
+        puf_ratios = pd.read_csv(
+            Path(CUR_PATH, "..", "puf_stage3", "puf_ratios.csv"), index_col=0
+        ).transpose()
+        new_records = tc.Records(
+            data=str(PUF_PATH), weights=puf_weights, adjust_ratios=puf_ratios
+        )
+        new_puf = tc.Calculator(records=new_records, policy=tc.Policy())
+        new_puf.advance_to_year(first_year)
+        new_puf.calc_all()
+        template_args, plot_paths = compare_calcs(
+            base_puf, new_puf, "puf", template_args, plot_paths
+        )
+    else:
+        msg = "PUF comparisons are not included in this report."
+        template_args["puf_msg"] = msg
+        template_args["puf_agg_plot"] = None
+        template_args["puf_combined_table"] = None
+        template_args["puf_income_table"] = None
+        template_args["puf_payroll_table"] = None
+
+    # # distribution plots
+    # dist_vars = [
+    #     ("c00100", "AGI Distribution"),
+    #     ("combined", "Tax Liability Distribution"),
+    # ]
+    # dist_plots = []
+    # for var, title in dist_vars:
+    #     plot = distplot(calcs, calc_labels, var, title=title)
+    #     img_path = Path(CUR_PATH, f"{var}_dist.png")
+    #     plot.save(str(img_path))
+    #     plot_paths.append(img_path)
+    #     dist_plots.append(f"![]({str(img_path)})" + "{.center}")
+    # template_args["cps_dist_plots"] = dist_plots
+
+    # # aggregate totals
+    # aggs = defaultdict(list)
+    # var_list = ["payrolltax", "iitax", "combined", "standard", "c04470"]
+    # for year in range(first_year, tc.Policy.LAST_BUDGET_YEAR + 1):
+    #     base_aggs = run_calc(base_cps, year, var_list)
+    #     new_aggs = run_calc(new_cps, year, var_list)
+    #     aggs["Tax Liability"].append(base_aggs["payrolltax"])
+    #     aggs["Tax"].append("Current Payroll")
+    #     aggs["Year"].append(year)
+    #     aggs["Tax Liability"].append(new_aggs["payrolltax"])
+    #     aggs["Tax"].append("New Payroll")
+    #     aggs["Year"].append(year)
+    #     aggs["Tax Liability"].append(base_aggs["iitax"])
+    #     aggs["Tax"].append("Current Income")
+    #     aggs["Year"].append(year)
+    #     aggs["Tax Liability"].append(new_aggs["iitax"])
+    #     aggs["Tax"].append("New Income")
+    #     aggs["Year"].append(year)
+    #     aggs["Tax Liability"].append(base_aggs["combined"])
+    #     aggs["Tax"].append("Current Combined")
+    #     aggs["Year"].append(year)
+    #     aggs["Tax Liability"].append(new_aggs["combined"])
+    #     aggs["Tax"].append("New Combined")
+    #     aggs["Year"].append(year)
+    # agg_df = pd.DataFrame(aggs)
+
+    # title = "Aggregate Tax Liability by Year"
+    # agg_chart = (
+    #     alt.Chart(agg_df, title=title)
+    #     .mark_line()
+    #     .encode(
+    #         x=alt.X(
+    #             "Year:O",
+    #             axis=alt.Axis(labelAngle=0, titleFontSize=20, labelFontSize=15),
+    #         ),
+    #         y=alt.Y(
+    #             "Tax Liability",
+    #             title="Tax Liability (Billions)",
+    #             axis=alt.Axis(titleFontSize=20, labelFontSize=15),
+    #         ),
+    #         color=alt.Color(
+    #             "Tax",
+    #             legend=alt.Legend(symbolSize=150, labelFontSize=15, titleFontSize=20),
+    #         ),
+    #     )
+    #     .properties(width=800, height=350)
+    #     .configure_title(fontSize=24)
+    # )
+    # img_path = Path(CUR_PATH, "agg_plot.png")
+    # agg_chart.save(str(img_path))
+    # plot_paths.append(img_path)
+    # template_args["agg_plot"] = f"![]({str(img_path)})" + "{.center}"
+
+    # # create tax liability tables
+    # template_args["combined_table"] = agg_liability_table(agg_df, "Combined")
+    # template_args["payroll_table"] = agg_liability_table(agg_df, "Payroll")
+    # template_args["income_table"] = agg_liability_table(agg_df, "Income")
 
     # write report and delete images used
     output_path = Path(CUR_PATH, "reports", f"taxdata_report_{date}.pdf")
